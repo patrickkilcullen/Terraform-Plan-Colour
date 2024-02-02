5fcf6bdb0277e782acb16cbd81be7130128f3b58
Patrick Kilcullen <patrick.kilcullen@hawkeyeinnovations.com>
2024-02-02 14:16:02 +0000

    Update colours

--- dist/index.js
+++ dist/index.js
@@ -24741,7 +24741,7 @@ function colourText(text) {
         return `\u001b[38;2;0;225;0m${text}`;
     }
     if (regexMinus.test(text)) {
-        return `\u001b[38;2;255;0;0m${text}`;
+        return `\u001b[38;2;255;31;31m${text}`;
     }
     if (regexTilde.test(text)) {
         return `\u001b[38;2;255;255;0m${text}`;
@@ -24760,7 +24760,7 @@ function run() {
             while (plan_array_filter.length != 0) {
                 var shift = plan_array_filter.shift();
                 if (shift !== undefined) {
-                    core.info(colourText(shift));
+                    out = out.concat(colourText(shift));
                 }
             }
         }
@@ -24769,18 +24769,7 @@ function run() {
         if (error instanceof Error)
             core.setFailed(error.message);
     }
-    // 3/4 bit
-    core.info('\u001b[35mThis foreground will be magenta');
-    // 8 bit
-    core.info('\u001b[38;5;6mThis foreground will be cyan');
-    // 24 bit
-    core.info('\u001b[38;2;255;0;0mThis foreground will be bright red');
-    // 3/4 bit
-    core.info('\u001b[43mThis background will be yellow');
-    // 8 bit
-    core.info('\u001b[48;5;6mThis background will be cyan');
-    // 24 bit
-    core.info('\u001b[48;2;255;0;0mThis background will be bright red');
+    core.info(out);
 }
 run();
 
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ function colourText(text: string): string {
         return `\u001b[38;2;0;225;0m${text}`
     }
     if(regexMinus.test(text)) {
-        return `\u001b[38;2;255;0;0m${text}`
+        return `\u001b[38;2;255;31;31m${text}`
     }
     if(regexTilde.test(text)) {
         return `\u001b[38;2;255;255;0m${text}`
@@ -33,30 +33,14 @@ function run() {
             while (plan_array_filter.length != 0) {
                 var shift = plan_array_filter.shift();
                 if (shift !== undefined) {
-                    core.info(colourText(shift))
+                    out = out.concat(colourText(shift))
                 }
             }
         }
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
     }
-    // 3/4 bit
-    core.info('\u001b[35mThis foreground will be magenta')
-
-    // 8 bit
-    core.info('\u001b[38;5;6mThis foreground will be cyan')
-
-    // 24 bit
-    core.info('\u001b[38;2;255;0;0mThis foreground will be bright red')
-
-    // 3/4 bit
-    core.info('\u001b[43mThis background will be yellow');
-
-    // 8 bit
-    core.info('\u001b[48;5;6mThis background will be cyan')
-
-    // 24 bit
-    core.info('\u001b[48;2;255;0;0mThis background will be bright red')
+    core.info(out)
 }
 
 run()
